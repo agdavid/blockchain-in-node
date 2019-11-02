e4284dd498792284874ea36eeadeb05ac5833888
agdavid <antonio.g.david@gmail.com>
2019-11-02 11:00:07 -0400

    generate genesis block

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const SHA256 = require("crypto-js/sha256");
 
-console.log(SHA256("Message!"));
+function calculateHash({ previousHash, timestamp, data, nonce = 1 }) {
+    return SHA256(previousHash + timestamp + JSON.stringify(data) + nonce).toString();
+}
+
+function generateGenesisBlock() {
+    const block = {
+        timestamp: + new Date(),
+        data: "Genesis Block",
+        previousHash: "0",
+    };
+    return {
+        ...block,
+        hash: calculateHash(block)
+    }
+}
+
+console.log(generateGenesisBlock());
+
