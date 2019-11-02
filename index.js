a1e8f043698568db8c44683669ff3d04d673e88d
agdavid <antonio.g.david@gmail.com>
2019-11-02 11:19:39 -0400

    validate chain

--- index.js
+++ index.js
@@ -63,3 +63,25 @@ function mineBlock(difficulty, block) {
     // run recursively until we get correct hash for block
     return trampoline(mine(nextNonce(block)));
 }
+
+// validate the chain
+// tce - tail call elimination
+function validateChain(chain) {
+    function tce(chain, index) {
+        // if genesis block, return true
+        if (index === 0) return true;
+        const { hash, ...currentBlockWithoutHash } = chain[index];
+        const currentBlock = chain[index];
+        const previousBlock = chain[index - 1];
+        // is current block hash valid
+        const isValidHash = (hash === calculateHash(currentBlockWithoutHash));
+        // is previous block hash valid
+        const isPreviousHashValid = (currentBlock.previousHash === previousBlock.hash);
+        // is chain valid until now
+        const isValidChain = (isValidHash && isPreviousHashValid);
+
+        if (!isValidChain) return false;
+        else return tce(chain, index - 1);
+    }
+    return tce(chain, chain.length - 1)
+}
